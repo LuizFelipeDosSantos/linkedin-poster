14787a7522bb78460d7e8942d4dcd29ab91fe52b
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2025-06-05 16:47:02 +0200

    Changed some layout

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const generateBtn = document.getElementById("generate");
 const topic = document.getElementById("topic");
-const post = document.getElementById("post");
+const post = document.getElementById("generated-post");
 
 const generatePost = () => {
+  post.innerText = `Post about ${topic.value} generated`;
   topic.value = "";
 }
 
