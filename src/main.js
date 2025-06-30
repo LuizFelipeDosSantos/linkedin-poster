1321de31a520cf7e51cb1ebfa772bc1b2e3cf8df
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2025-06-30 12:06:07 +0200

    Client Done

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+import axios from 'axios';
+import { API_BASE_URL } from "./consts.js";
+
 const generateBtn = document.getElementById("generate");
 const topic = document.getElementById("topic");
 const post = document.getElementById("generated-post");
 
-const generatePost = () => {
-  post.innerText = `Post about ${topic.value} generated`;
-  topic.value = "";
+const generatePost = async () => {
+  post.innerText = 'Generating Post...';
+  try {
+    let response = await axios.get(API_BASE_URL + `/post/${topic.value}`);
+    response = await response.data;
+    post.innerText = response;
+    topic.value = "";
+  } catch (error) {
+    post.innerHTML = 'Something went wrong :(';
+    console.error(error);
+  }
 }
 
 generateBtn.addEventListener("click", generatePost);
